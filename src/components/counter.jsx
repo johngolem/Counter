16bb4c7570e6e9646cd3aeb9073546bbd9ff39e6
John Paul <johnpaul@trees.local>
2022-03-21 09:35:51 +0300

    conditional render, an if statement function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,12 +10,20 @@ class Counter extends Component {
         imageUrl:'https://picsum.photos/200',
         tags:['tag 1','tag 2', 'tag 3']
     };
+
+    renderTags () {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>
+                    {this.state.tags.map(tag=> <li key={tag.id}>{ tag }</li>)}
+            </ul>
+    }
+
+
     render() { 
         return (<React.Fragment>
             <button className='btn btn-secondary btn-sm'>Increment</button> 
-            <ul>
-                {this.state.tags.map(tag=> <li key={tag.id}>{ tag }</li>)}
-            </ul>
+            {this.renderTags()}
             </React.Fragment>
     ); 
 }
