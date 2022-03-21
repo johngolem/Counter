17e6cdcedb6f5fe86286315b5ce4f2f49b322ec0
John Paul <johnpaul@trees.local>
2022-03-21 09:30:32 +0300

    conditional render branch

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,8 +12,6 @@ class Counter extends Component {
     };
     render() { 
         return (<React.Fragment>
-            <img src={this.state.imageUrl} alt=' ' />
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button> 
             <ul>
                 {this.state.tags.map(tag=> <li key={tag.id}>{ tag }</li>)}
@@ -22,17 +20,6 @@ class Counter extends Component {
     ); 
 }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-formatCount() {
-
-    const {count} = this.state;
-    return count === 0 ? "Zero" : count;
-}
 }
     
 export default Counter;   
